5c662ab76ad9e27a0358d423ebd6aad6815e0c00
piedcipher <pateltirth454@gmail.com>
2022-07-20 01:17:00 +0530

    used MutationObserver for dom changes in attributes

--- background.js
+++ background.js
@@ -1,21 +1,26 @@
 let muted = false;
 
-setInterval(() => {
-    const nowPlayingBar = document.querySelector(".Root__now-playing-bar");
-    const volumeRocker = document.querySelector(".volume-bar__icon-button");
-    console.log(nowPlayingBar.children);
-    if (nowPlayingBar?.children?.length === 1 && nowPlayingBar?.firstChild?.hasAttribute("data-testadtype")) {
-        if (nowPlayingBar.firstChild.getAttribute("data-testadtype") !== "ad-type-none") {
-            console.log("ad is playing");
-            if (!muted) {
-                volumeRocker?.click();
-                muted = true;
+let observer = new MutationObserver(mutations => {
+    for (let mutation of mutations) {
+        if (mutation.attributeName === "data-testadtype") {
+            const nowPlayingBar = document.querySelector(".Root__now-playing-bar");
+            const volumeRocker = document.querySelector(".volume-bar__icon-button");
+            if (nowPlayingBar?.children?.length === 1 && nowPlayingBar?.firstChild?.hasAttribute("data-testadtype")) {
+                if (nowPlayingBar.firstChild.getAttribute("data-testadtype") !== "ad-type-none") {
+                    console.log("ad is playing");
+                    if (!muted) {
+                        volumeRocker?.click();
+                        muted = true;
+                    }
+                } else {
+                    if (muted) {
+                        volumeRocker?.click();
+                    }
+                    muted = false;
+                }
             }
-        } else {
-            if (muted) {
-                volumeRocker?.click();
-            }
-            muted = false;
         }
     }
-}, 2000);
+ });
+
+ observer.observe(document, { childList: true, subtree: true, attributes: true });
